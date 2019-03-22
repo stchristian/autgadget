7c9291732f7326b178f683b89015c88280ab89af
Hübner Krisztián <hubner.krisztian97@gmail.com>
2019-03-22 19:41:15 +0100

    Paraméterezhető és async lett a felhasználók lekérése

--- middlewares/user/getUserList.js
+++ middlewares/user/getUserList.js
@@ -2,10 +2,10 @@ const requireOption = require('../common').requireOption;
 /**
  * Lekéri az felhasználók listáját.
  */
-module.exports = (objectRepository) => {
+module.exports = (objectRepository, filter) => {
     const userModel = requireOption(objectRepository, 'userModel');
 
-    return (req,res,next) => {
+    return async (req,res,next) => {
         const pageNo = parseInt(req.query.pageNo) || 1;
         const pageSize = 5;
 
@@ -14,31 +14,27 @@ module.exports = (objectRepository) => {
             limit : pageSize
         };
 
-        if(typeof req.query.kereses === 'string' && req.query.kereses != '' ) {
-            userModel.countDocuments( { $text : {$search : req.query.kereses }}, (err, totalCount) => {
-                userModel.find( { $text : {$search : req.query.kereses }}, {}, query)
-                    .exec( (err, users) => {
-                        if(err) next(err);
-                        res.locals.kereses = req.query.kereses;
-                        res.locals.users = users; 
-                        res.locals.totalCount = totalCount;
-                        res.locals.totalPages = Math.ceil(totalCount / pageSize);
-                        next();
-                    });
-            });
+        let options = {};
+        if(typeof filter !== 'undefined') {
+            options = filter;
         }
-        else {
-            userModel.countDocuments((err, totalCount) => {
-                userModel.find({}, {}, query)
-                .exec((err, users) => {
-                    if(err) next(err);
-                    res.locals.users = users;
-                    res.locals.totalCount = totalCount;
-                    res.locals.totalPages = Math.ceil(totalCount / pageSize);
-                    //res.locals.totalPages = 10;
-                    next();
-                });
-            });
+
+        if(typeof req.query.kereses === 'string' && req.query.kereses != '' ) {
+            options.$text = { $search : req.query.kereses };
+            res.locals.kereses = req.query.kereses;
+        }
+        
+        try {
+            let totalCount = await userModel.countDocuments(options);
+            let users = await userModel.find(options, {}, query);
+    
+            res.locals.users = users; 
+            res.locals.totalCount = totalCount;
+            res.locals.totalPages = Math.ceil(totalCount / pageSize);
+            return next();
+        }
+        catch(err) {
+            return next(err);
         }
     };
 };
